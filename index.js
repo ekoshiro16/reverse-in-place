002f3e86dbe0888998181f2fbe02245c4985c698
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-29 19:42:17 -0700

    added example

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ Write an algorithm that accepts an array of characters and reverses the characte
 Note: An in-place algorithm is one that modifies the original input.
 In an in-place algorithm, you may NOT create any additional variables that are arrays or objects. 
 In-place algorithms may, however, use variables that are primitive data types (i.e. numbers, booleans, etc.)
+
+For example:
+For the input of ['h', 'e', 'l', 'l', 'o'],
+your algorithm would output ['o', 'l', 'l', 'e', 'h']
+
 */
 
 // Write your code below: 
